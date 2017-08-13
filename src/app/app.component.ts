a5a20fcab34c6f6f11a44658007b62aa95857157
Jhonatan Giraldo <jhonatangiraldo111@gmail.com>
2017-08-13 18:44:44 -0500

    Examples done. Html with flexbox and differents behaviors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  /**
+   * data to display in the view
+   */
+  private data: number[];
+  
+  constructor() {}
+
+  ngOnInit() {
+    this.fillData();
+  }
+
+  private fillData() {
+    this.data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
